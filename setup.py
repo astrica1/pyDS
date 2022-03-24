dd2b1a9b6ccbb3785bfd0cf609e023c5a4b38385
astrica1 <alireza.matani.1376@gmail.com>
2022-03-24 10:36:30 +0430

    initializing repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyds',
+    packages=find_packages(include=['pyds']),
+    version='0.1.0',
+    description='Python Data Structures Library',
+    author='astrica1',
+    license='MIT',
+)
