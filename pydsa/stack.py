898c77f2b5691566041bd2e2e5d6834ff6219f04
astrica1 <alireza.matani.1376@gmail.com>
2022-04-14 19:38:39 +0430

    add stack data structure

---
+++ pydsa/stack.py
@@ -0,0 +1,65 @@
+class Stack:
+    def __init__(self, max_size: int = None):
+        self._items = []
+        self._pointer = 0
+        self._max_size = max_size
+    
+    def IsFull(self):
+        """ Returns True if the stack is full, False otherwise """
+        return self._max_size is not None and self._pointer == self._max_size
+    
+    def IsEmpty(self):
+        """ Returns True if the stack is empty, False otherwise """
+        return self._pointer == 0
+    
+    def Push(self, item):
+        """ Pushes an item onto the stack
+        
+        Args:
+            item: The item to be pushed onto the stack
+        """
+        if self.IsFull():
+            raise Exception('Stack is full')
+        self._items.append(item)
+        self._pointer += 1
+    
+    def Pop(self):
+        """ Pops an item off the stack
+        
+        Returns:
+            The item popped off the stack
+        """
+        if self.IsEmpty():
+            raise Exception('Stack is empty')
+        self._pointer -= 1
+        return self._items.pop()
+    
+    def Peek(self):
+        """ Returns the top item on the stack without removing it
+        
+        Returns:
+            The top item on the stack
+        """
+        if self.IsEmpty():
+            raise Exception('Stack is empty')
+        return self._items[-1]
+    
+    def __len__(self):
+        return self._pointer
+    
+    def __str__(self):
+        return str(self._items)
+    
+    def __repr__(self):
+        return '<class \'stack\'> : ' + str(self._items)
+    
+    def __iter__(self):
+        return self._items.__reversed__().__iter__()
+    
+    def __next__(self):
+        if len(self._items) == 0:
+            raise StopIteration
+        return self._items.pop()
+    
+def new():
+    return Stack()
